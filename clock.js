433daf2ef6f473ffbfa9a151a283b8a805d50931
ssongseulki <ssongseulki@naver.com>
2020-12-10 18:25:00 +0900

    add_CSS

--- clock.js
+++ clock.js
@@ -5,8 +5,9 @@ function getTime(){
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`
+    // const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes}` : minutes} `;
+    // : ${seconds < 10 ? `0${seconds}` : seconds}
         //10 보다 작은 숫자가 오면 0+숫자로 표현될 수 있는 삼항연산자
 }
 
